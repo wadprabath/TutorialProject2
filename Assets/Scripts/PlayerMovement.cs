d101f177dd20b56f0735b57b970d51da910c4f0d
Your Name <wadprabath@gmail.com>
2021-12-02 23:05:11 +0200

    TutorialProject2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+    AudioSource m_AudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //"Get a reference to a component of type 'Animator', and assign it to the variable called m_Animator."
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool horizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool verticalInput = !Mathf.Approximately(vertical, 0f);
+
+        //if true, walking
+        bool isWalking = horizontalInput || verticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+       Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+
+       m_Rotation = Quaternion.LookRotation(desiredForward);
+
+       
+     
+    }
+
+
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+
+    
+}
